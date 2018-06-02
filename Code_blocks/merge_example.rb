c5ee4ff1b4225c8c8b3d87c89879c16bbd4f94fc
anum <anumsharma09@gmail.com>
2018-06-02 01:18:57 -0700

    feat: merge examples

--- Code_blocks/merge_example.rb
+++ Code_blocks/merge_example.rb
@@ -7,3 +7,21 @@ h2 = { "b" => 111, "c" => 222 }
 puts h1.merge(h2)
 
 puts h2.merge(h1)
+
+puts h1.merge(h2) { |key,old,new| new }
+
+puts h1.merge(h2) { |key,old,new| old }
+
+puts h1.merge(h2) { |key,old,new| old * 2 }
+
+h1.merge(h2) do |key, old,new|
+    if old<new
+        puts old
+    else
+        puts new
+    end
+end
+
+puts h1.merge(h2) {|k,o,n| o < n ? o : n}
+
+h1.merge!(h2)
