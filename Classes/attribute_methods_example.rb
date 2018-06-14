b6b5ac49b99430639e222bc22126311a15240404
anum <anumsharma09@gmail.com>
2018-06-13 23:28:12 -0700

    edit: correct the reader and writer method

--- Classes/attribute_methods_example.rb
+++ Classes/attribute_methods_example.rb
@@ -2,31 +2,35 @@
      attr_accessor :name
      attr_writer :color
      attr_reader :legs, :arms
-     
+    
+     # make a writer for legs and arms
+    def setup_limbs
+         @legs = 4
+         @arms = 0
+     end 
+
+     # writer method
      def noise=(noise)
          @noise=noise
      end
      
-     def setup_limbs
-         @legs = 4
-         @arms = 0
-     end 
-     
+    # writer method   
      def noise
          @noise
      end
      
+     # reader method
      def color
          "the color is #{@color}."
+     end
  end
 
 animal1 = Animal.new
-animal1.setup_limbs
+animal1.setup_limbs  # calling writer method
 animal1.noise="Moo!"
+puts animal1.noise
 animal1.name= "Steve"
 puts animal1.name
-puts animal1.noise
 animal1.color="Black"
 puts animal1.color
-puts animal.legs
-puts animal1.noise
+puts animal1.legs
